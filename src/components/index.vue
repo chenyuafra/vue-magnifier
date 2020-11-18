d17525da8c20d8ca040cd2af88bf4de1771b0012
zhangchenyu <969231560@qq.com>
2020-11-18 14:05:19 +0800

    初始提交

---
+++ src/components/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+<script>
+  export default {
+    name: '',
+    data() {
+      return {
+
+      }
+    },
+    // 模板渲染前钩子函数
+    created() {
+
+    },
+    // 模板渲染后钩子函数
+    mounted() {
+
+    },
+    methods: {
+
+    },
+  }
+</script>
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ImgEnlarge from '@/components/img-enlarge.vue'
-
+import Home from '@/components/index.vue'
+import ImgEnlarge from '@/view/img-enlarge.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'ImgEnlarge',
-      component: ImgEnlarge
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: home
+  }, {
+    path: '/imgenlarge',
+    name: 'ImgEnlarge',
+    component: ImgEnlarge
+  }]
 })
